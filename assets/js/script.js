25e348ba8dfff60276258539e9e37d8c27352805
sabri chafroud <chafroud.sabri@hotmail.fr>
2023-10-16 14:37:15 +0200

    16/10/23 14h37

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 // test java script si bien chargé dans ma page html
 console.log("ça marche au boulot^^");
 
+// Array pour stocker les opérations
+const arraySaisies = [];
+
 // je crée une variable pour mes signes
 let lesSignes = ["+","-","*","/"];
-
-// creer variable bouton egale
-let egale = "=";
 
 // je crée variable bouton "."
 let point = ".";
 
-// variable bouton "c"
-let C ="c";
+
 
 //              CREE VARIABLES POUR LA STRUCTURE DE MA CALCULATRICE
 
@@ -24,6 +23,7 @@ let ecran = document.createElement("div");
 ecran.classList.add("ecran");
 ecran.innerText = 0;
 calculatrice.appendChild(ecran);
+           
 
 //clavier
 let clavier = document.createElement("div");
@@ -58,29 +58,55 @@ clavierSymboles.appendChild(bouton);
 
 // variable creer en haut puis faire le bouton"." enfant de clavier
 let btnPoint = document.createElement("button")
-btnPoint.classList.add("btn-point");
+btnPoint.classList.add("btn-point","btn");
 btnPoint.innerText = point;
 clavierChiffres.appendChild(btnPoint);
+
 
 // variable creer en haut faire le bouton "c" enfant de clavierChiffres
 let btnC = document.createElement("button");
 btnC.classList.add("btnC");
-btnC.innerText = C;
-clavierChiffres.appendChild(btnC)
+btnC.innerText="AC";
+clavierChiffres.appendChild(btnC);
+// bouton AC reset l'affichage 
+btnC.addEventListener("click", () => {
+    document.querySelector(".ecran").innerText = "";
+    if (arraySaisies.length == 0) {
+            document.querySelector(".ecran").innerText = ""; 
+        }
+} )
 
 // variable creer en haut faire le bouton "=" enfant de clavierChiffres
 let btnEgale = document.createElement("button");
 btnEgale.classList.add("btn-egale");
-btnEgale.innerText = egale;
+btnEgale.innerText = "=";
 clavierChiffres.appendChild(btnEgale);
+// calcule simple 
+ let calcul = document.querySelector(".btn-egale");
+     calcul.addEventListener('click', () => {
+         document.querySelector(".ecran").innerText = 
+         eval(document.querySelector(".ecran").innerText);
+     })
 
 // inscrire valeur seclectionner dans l'affichage
-let mesBoutons = document.querySelectorAll("button");
-     for (let index = 0; index <= mesBoutons.length; index++) {
-     mesBoutons[index].addEventListener('click', eventClick => {
-     document.querySelector(".ecran").innerText += eventClick.target.innerText;
-        })                    
-     }
+let mesBoutons = document.querySelectorAll(".btn");
+console.log(mesBoutons);
+mesBoutons.forEach(unBouton => {
+    unBouton.addEventListener("click", function() {
+        if (arraySaisies.length == 0) {
+            document.querySelector(".ecran").innerText = ""; 
+        }
+        document.querySelector(".ecran").innerText += unBouton.innerText;
+            arraySaisies.push(unBouton.innerText);
+            console.log(arraySaisies);
+            console.log("Vous avez cliqué sur : ", unBouton.innerText);      
+    })
+})
+    //  for (let index = 0; index <= mesBoutons.length; index++) {
+    //  mesBoutons[index].addEventListener('click', eventClick => {
+    //  document.querySelector(".ecran").innerText += eventClick.target.innerText;
+    //     })                    
+    //  }
 
 
 
